d8b6a20abae970685ceeab0c1b2ae6565f55c46f
KevinBueno03 <kbueno@unah.hn>
2021-10-28 13:46:03 -0600

    ruta auth
    Se agrego la ruta auth

--- BackEnd/routes/router.js
+++ BackEnd/routes/router.js
@@ -5,9 +5,22 @@ const ctrlPatient = require("../controllers/patients");
 const ctrlDoctor = require("../controllers/doctors");
 const verifyAccount = require("../controllers/verify_account");
 const userLogin = require("../controllers/login");
+const auth = require("../controllers/auth");
+
 
 router.post("/register-patient", ctrlPatient.register);
+router.post("/patients", ctrlPatient.listALL);
+router.post("/patients/:id", ctrlPatient.listOne);
+
 router.post("/register-doctor", ctrlDoctor.register);
+
+
 router.post("/login", userLogin.login);
 router.get("/verifyaccount/:code", verifyAccount.verify);
+
+router.post("/auth",auth,(req,res)=>{
+    res.status(200).send("Entro al token y es valido")
+})
+
+
 module.exports = router;
