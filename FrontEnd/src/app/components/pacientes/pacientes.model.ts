0b55cf8c1214e54a2d8eb2563262e3b311fbf874
KevinBueno03 <kbueno@unah.hn>
2021-11-02 21:40:58 -0600

    separacion de repositorios

--- FrontEnd/src/app/components/pacientes/pacientes.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Pacientes{
-  firstName: string;
-  firstLastName: string;
-  hn_id:string;
-  email: string;
-}
--- FrontEnd/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: `http://3.21.102.107:4200`,
-  APIBASEURL:`http://3.21.102.107:8080/api`
-};
